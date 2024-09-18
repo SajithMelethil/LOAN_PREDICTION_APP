2e49c15e52652c7801b513ec97654dd491831649
SajithMelethil <118508251+SajithMelethil@users.noreply.github.com>
2024-09-18 13:54:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,106 +1,83 @@
-# save this as app.py
-from flask import Flask, escape, request, render_template
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
-model = pickle.load(open('MLMODEL.pkl', 'rb'))
+
+# Load the model once at startup
+with open('MLMODEL.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 @app.route('/')
-def home():
+def home() -> str:
     return render_template("index.html")
 
-
 @app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method ==  'POST':
-        Gender = request.form['Gender']
-        Married = request.form['Married']
-        Dependents = request.form['Dependents']
-        Education = request.form['Education']
-        Employed = request.form['Self_Employed']
-        Credithistory = float(request.form['credithistory'])
-        Area = request.form['Property_Area']
-        ApplicantIncome = float(request.form['Applicantincome'])
-        CoapplicantIncome = float(request.form['Coapplicantincome'])
-        LoanAmount = float(request.form['LoanAmount'])
-        Loan_Amount_Term = float(request.form['Loan_Amount_Term'])
+def predict() -> str:
+    if request.method == 'POST':
+        # Collect form data
+        form_data = {
+            'Gender': request.form['Gender'],
+            'Married': request.form['Married'],
+            'Dependents': request.form['Dependents'],
+            'Education': request.form['Education'],
+            'Self_Employed': request.form['Self_Employed'],
+            'Credithistory': float(request.form['credithistory']),
+            'Property_Area': request.form['Property_Area'],
+            'ApplicantIncome': float(request.form['Applicantincome']),
+            'CoapplicantIncome': float(request.form['Coapplicantincome']),
+            'LoanAmount': float(request.form['LoanAmount']),
+            'Loan_Amount_Term': float(request.form['Loan_Amount_Term']),
+        }
 
-        # gender
-        if (Gender == "Male"):
-            Gender_Male=1
-        else:
-            Gender_Male=0
+        # Convert categorical variables to binary variables
+        Gender_Male = 1 if form_data['Gender'] == "Male" else 0
+        Married_Yes = 1 if form_data['Married'] == "Yes" else 0
+        Education_Not_Graduate = 1 if form_data['Education'] == "Non-graduated" else 0
+        Self_Employed_Yes = 1 if form_data['Self_Employed'] == "Yes" else 0
+        Property_Area_Urban = 1 if form_data['Property_Area'] == "Urban" else 0
         
-        # married
-        if(Married=="Yes"):
-            Married_Yes = 1
-        else:
-            Married_Yes=0
+        # Dependents encoding
+        Dependents_1 = int(form_data['Dependents'] == '1')
+        Dependents_2 = int(form_data['Dependents'] == '2')
+        Dependents_3 = int(form_data['Dependents'] == '3+')
 
-        # dependents
-        if(Dependents=='1'):
-            Dependents_1 = 1
-            Dependents_2 = 0
-            Dependents_3 = 0
-        elif(Dependents == '2'):
-            Dependents_1 = 0
-            Dependents_2 = 1
-            Dependents_3 = 0
-        elif(Dependents=="3+"):
-            Dependents_1 = 0
-            Dependents_2 = 0
-            Dependents_3 = 1
-        else:
-            Dependents_1 = 0
-            Dependents_2 = 0
-            Dependents_3 = 0  
+        # Log transformations
+        ApplicantIncomelog = np.log(form_data['ApplicantIncome'])
+        CoapplicantincomeLog = np.log(1 + form_data['CoapplicantIncome'])
+        LoanAmountlog = np.log(form_data['LoanAmount'])
+        Loan_Amount_Termlog = np.log(form_data['Loan_Amount_Term'])
 
-        # education
-        if (Education=="Non-graduated"):
-            Education_Not_Graduate	=1
-        else:
-            Education_Not_Graduate	=0
+        # Prepare input features for prediction
+        features = [
+            form_data['Credithistory'], 
+            ApplicantIncomelog,
+            LoanAmountlog, 
+            Loan_Amount_Termlog, 
+            CoapplicantincomeLog, 
+            Gender_Male, 
+            Married_Yes, 
+            Dependents_1, 
+            Dependents_2, 
+            Dependents_3,
+            Education_Not_Graduate, 
+            Self_Employed_Yes, 
+            Property_Area_Urban
+        ]
 
-        # employed
-        if (Employed == "Yes"):
-            Self_Employed_Yes=1
-        else:
-            Self_Employed_Yes=0
+        # Make prediction
+        prediction = model.predict([features])[0]
 
-        # property area
+        # Map prediction to the output string
+        prediction_text = "No" if prediction == "N" else "Yes"
 
-        if(Area=="Yes"):
-            Property_Area_Urban=1
-        else:
-            Property_Area_Urban=0
-        
+        return render_template("prediction.html", prediction_text=f"--- LOAN STATUS IS --- {prediction_text}")
 
+    return render_template("prediction.html")
 
-        ApplicantIncomelog = np.log(ApplicantIncome)
-        CoapplicantincomeLog = np.log(1 + CoapplicantIncome)
-        LoanAmountlog = np.log(LoanAmount)
-        Loan_Amount_Termlog = np.log(Loan_Amount_Term)
-
-        prediction = model.predict([[ Credithistory, ApplicantIncomelog,LoanAmountlog, Loan_Amount_Termlog, CoapplicantincomeLog, Gender_Male, Married_Yes, Dependents_1, Dependents_2, Dependents_3,Education_Not_Graduate, Self_Employed_Yes, Property_Area_Urban]])
-
-        # print(prediction)
-
-        if(prediction=="N"):
-            prediction="No"
-        else:
-            prediction="Yes"
-
-
-        return render_template("prediction.html", prediction_text="--- LOAN STATUS IS ---{}".format(prediction))
-
-
-
-
-    else:
-        return render_template("prediction.html")
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     app.run(debug=True)
